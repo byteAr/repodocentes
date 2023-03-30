879ad29dd8961c7c7d9753a131f1cc56062cd03a
Marcos lopez <marcosmatiaslopez.2018@gmail.com>
2023-03-30 12:04:05 -0300

    componente de bibliotecas actualizado para verse en testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   /* baseUrl: 'http://10.98.40.24:5000' */
-  baseUrl: 'http://localhost:5000'
-  /* baseUrl: 'https://servicioiugna.iugna.edu.ar' */
+  /* baseUrl: 'http://localhost:5000' */
+  baseUrl: 'https://servicioiugna.iugna.edu.ar'
 };
